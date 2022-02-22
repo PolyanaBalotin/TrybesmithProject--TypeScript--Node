fe52d003c628c0d1f8369fb12a23659f71118b44
Polyana Balotin <polybalotin@hotmail.com>
2022-02-22 20:04:10 -0300

    adiciona token

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,12 @@
+import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import UserServicer from '../services/UserService';
 
 export const createUser = async (req: Request, res: Response) => {
   const { username, classe, level, password } = req.body;
-  const data = await UserServicer.create({ username, classe, level, password });
-  return res.status(201).json(data);
+  await UserServicer.create({ username, classe, level, password });
+  const token = jwt.sign({ username }, process.env.JWT_SECRET || 'segredo');
+  return res.status(201).json({ token });
 };
 
 export default {
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,7 @@ import UserModel from '../models/UserModel';
 import { IUser } from '../models/IUser';
 
 const create = async ({ username, classe, level, password }: IUser) => {
-  const user = UserModel.create({ username, classe, level, password });
+  const user = await UserModel.create({ username, classe, level, password });
   return user;
 };
 
